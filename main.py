5789ec6336b77680f7b5fa3c8698989c1f95179c
pezhman <p.sharafdin@gmail.com>
2022-09-29 12:53:17 -0400

    main as a asyncio function added to remove the while true loop

--- main.py
+++ main.py
@@ -8,22 +8,24 @@ from datetime import datetime
 DeviceManager = DeviceManager
 BLEsensor = RecievinBLE_sensor
 
-
-while(True):
+async def main():
+    while True:
         jSON_msg = None
         try:
             # Scanning BLEs to find the sensor, connect and read data
-            jSON_msg = asyncio.run(BLEsensor.scanner())
+            jSON_msg = await BLEsensor.scanner()
 
         except Exception as Argument:
             print(Argument)
 
-        print(type(jSON_msg),jSON_msg)
+        print(type(jSON_msg), jSON_msg)
         if jSON_msg is not None:
             try:
                 # send data to Azure IoT hub
-                asyncio.run(DeviceManager.send_msg(jSON_msg))
+                await DeviceManager.send_msg(jSON_msg)
             except Exception as Argument:
                 print("the device is created in IoT hub")
                 print(Argument)
         time.sleep(6)
+
+asyncio.run(main())
